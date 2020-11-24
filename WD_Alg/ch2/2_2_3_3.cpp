4dbbf908ba5a37c311666ef83797f6b96aacd85d
ccccqyc <chuancnw@yeah.net>
2020-11-24 23:59:14 +0800

    删除值x

---
+++ WD_Alg/ch2/2_2_3_3.cpp
@@ -0,0 +1,10 @@
+void del_x(Sqllist &L, ElemType x) {
+    int k = 0;
+    for (int i = 0; i < L.length; i++) {
+        if (L.data[i] != x) {
+            L.data[k] = L.data[i];
+            k++;
+        }
+    }
+    L.length = k;
+}
